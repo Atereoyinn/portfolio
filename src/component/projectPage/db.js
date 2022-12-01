fd11eb7dd721ecd4851fe5a1e2c410620865dde1
Atereoyinn <98267555+Atereoyinn@users.noreply.github.com>
2022-11-30 17:15:50 -0800

    I removed conflicting data

--- src/component/projectPage/db.js
+++ src/component/projectPage/db.js
@@ -1,37 +1,26 @@
 export const database = [
   {
-<<<<<<< HEAD
-    id:1,
-    name: "SmartLign Blogspot",
-    image: "https://ipfs.filebase.io/ipfs/QmSKZp6XS42mN5oWqf6eajvxcwKYBga6EZwD11WGQpzdRv",
-    link: "https://smartlign-test-test-deploy.netlify.app/",
-=======
     id: 1,
-    name: "Nairaland Landing Page",
+    name: "SmartLign Blogspot",
     image:
-      "https://ipfs.filebase.io/ipfs/bafybeic5quqhutu4x3xtr3imk2v2djkonk6stuybh3z4bwbtd6azrmz3iu",
-    link: "https://nairalandd.netlify.app/",
->>>>>>> b20a62c0906de29981cd1d0f473b3872d8d045f1
+      "https://ipfs.filebase.io/ipfs/QmSKZp6XS42mN5oWqf6eajvxcwKYBga6EZwD11WGQpzdRv",
+    link: "https://smartlign-test-test-deploy.netlify.app/",
   },
   {
     id: 2,
     name: "Github Profile Finder",
     image:
       "https://ipfs.filebase.io/ipfs/bafybeian3k6rikzanzeiheic5pzp3n3cd5i7fum3nq5quuzkiqd4ihhebu",
-<<<<<<< HEAD
     link: "https://profilefinderr.netlify.app/",
-=======
-    link: "https://atere-github-profilefinder.netlify.app/",
->>>>>>> b20a62c0906de29981cd1d0f473b3872d8d045f1
   },
   {
-    id:3,
+    id: 3,
     name: "Crypto Data Tracker",
-    image: "https://ipfs.filebase.io/ipfs/QmZntoCiRUuUXDmNtnrbrqfUNcVC9k7gEhrZ8j2dZQzJud",
+    image:
+      "https://ipfs.filebase.io/ipfs/QmZntoCiRUuUXDmNtnrbrqfUNcVC9k7gEhrZ8j2dZQzJud",
     link: "https://atere-cryptotracker.netlify.app/",
   },
   {
-<<<<<<< HEAD
     id: 4,
     name: "Nairaland Landing Page",
     image:
@@ -39,27 +28,23 @@ export const database = [
     link: "https://nairalandd.netlify.app/",
   },
   {
-    id:5,
+    id: 5,
     name: "Twitter-clone",
-    image: "https://ipfs.filebase.io/ipfs/QmNXVChiScZ3x6uV6Utwa9jnb1FnjGCvN3m4cbsi1gFPsM",
+    image:
+      "https://ipfs.filebase.io/ipfs/QmNXVChiScZ3x6uV6Utwa9jnb1FnjGCvN3m4cbsi1gFPsM",
     link: "https://tweetta.netlify.app/",
   },
   {
     id: 6,
     name: "Movie Application",
-    image:
-      "Movie-app.png",
+    image: "Movie-app.png",
     link: "moviee-cinema.netlify.app",
-=======
-    id:4,
-    name: "Nairaland Landing Page",
-    image: "dgdjhd",
-    link: "",
   },
   {
-    id:5,
+    id: 5,
     name: "Crypto Data Tracker",
-    image: "https://ipfs.filebase.io/ipfs/QmZntoCiRUuUXDmNtnrbrqfUNcVC9k7gEhrZ8j2dZQzJud",
+    image:
+      "https://ipfs.filebase.io/ipfs/QmZntoCiRUuUXDmNtnrbrqfUNcVC9k7gEhrZ8j2dZQzJud",
     link: "https://atere-cryptotracker.netlify.app/",
   },
   {
@@ -68,6 +53,5 @@ export const database = [
     image:
       "https://ipfs.filebase.io/ipfs/bafybeian3k6rikzanzeiheic5pzp3n3cd5i7fum3nq5quuzkiqd4ihhebu",
     link: "https://atere-github-profilefinder.netlify.app/",
->>>>>>> b20a62c0906de29981cd1d0f473b3872d8d045f1
   },
 ];
